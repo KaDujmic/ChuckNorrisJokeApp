67df060086c5ba1ace5d4d88189bb81f509c326d
Karlo Dujmic <kd48376@oss.unist.hr>
2023-03-09 13:25:16 +0100

    Add auth and user router, error middleware to app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const userRouter = require('./router/userRouter');
+const authRouter = require('./router/authRouter');
+const { errorMiddleware } = require('./utils/callbackErrorHandler');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan('dev'));
 
+app.use('/user', userRouter);
+app.use('/', authRouter);
+
+app.use(errorMiddleware);
+
 app.listen(PORT, () => {
-  console.log(`Example app listening at ${PORT}`);
+  console.log(`App listening at ${PORT}`);
 });
