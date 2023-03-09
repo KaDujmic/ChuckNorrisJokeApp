522c6d871cf76122815af2ae71bb45911dab4770
Karlo Dujmic <kd48376@oss.unist.hr>
2023-03-09 14:00:30 +0100

    Create function to fetch random joke and send the email

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 const models = require('../models');
 const Model = require('./crudController');
+const { generateMail } = require('../utils/email');
+const axios = require('axios');
 
 exports.findAllUsers = async (req, res) => {
   await Model.findAllModel(models.user, req, res);
@@ -19,4 +21,14 @@ exports.updateUser = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
   await Model.deleteModel(models.user, req, res);
+};
+
+exports.getJoke = async (req, res) => {
+  const joke = await axios.get('https://api.chucknorris.io/jokes/random');
+  console.log(joke);
+  res.status(200).json({
+    user: res.locals.user,
+    joke: joke.data.value
+  });
+  generateMail(res.locals.user, joke.data.value);
 };
