9613c199a7560dded9b22c4fc315ff653623c434
Karlo Dujmic <kd48376@oss.unist.hr>
2023-03-09 14:42:14 +0100

    Restrict crud routes for user to admin only

--- router/userRouter.js
+++ router/userRouter.js
@@ -10,21 +10,26 @@ router.use(callbackErrorHandler(authController.isLoggedIn));
 router
   .route('/')
   .get(
+    authController.restrictTo(1),
     callbackErrorHandler(userController.findAllUsers)
   )
   .post(
+    authController.restrictTo(1),
     callbackErrorHandler(userController.createUser)
   );
 
 router
   .route('/:email')
   .get(
+    authController.restrictTo(1),
     callbackErrorHandler(userController.findUser)
   )
   .put(
+    authController.restrictTo(1),
     callbackErrorHandler(userController.updateUser)
   )
   .delete(
+    authController.restrictTo(1),
     callbackErrorHandler(userController.deleteUser)
   );
 
