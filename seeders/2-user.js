1bf6414a7809d95e4e14eaf12f947db0637788b6
Karlo Dujmic <kd48376@oss.unist.hr>
2023-03-09 14:41:14 +0100

    Update seeder files

--- seeders/2-user.js
+++ seeders/2-user.js
@@ -9,12 +9,21 @@ module.exports = {
         last_name: 'Doe',
         email: 'john@example.com',
         password: '$2a$12$uvZgtrfGsWx5ZP6z7cbz4.GPP.pOLGe9zJ/tCqinBSLz58QxnVtRC',
+        role_id: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        first_name: 'Mike',
+        last_name: 'Smith',
+        email: 'mike@example.com',
+        password: '$2a$12$uvZgtrfGsWx5ZP6z7cbz4.GPP.pOLGe9zJ/tCqinBSLz58QxnVtRC',
+        role_id: 2,
         created_at: new Date(),
         updated_at: new Date()
       }
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
-  }
+  async down (queryInterface, Sequelize) {}
 };
